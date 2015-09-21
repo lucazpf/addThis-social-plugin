48627c7cde7426f7f32683739d7a9aba13e6ebcb
Luca Scalvi <luca@scalvi.it>
2015-09-22 00:02:27 +0200

    First step

---
+++ magic.js
@@ -0,0 +1,25 @@
+
+    $(document).ready(function() {
+        $('#lolpin').click(function() {
+                $('#pin-button').fadeToggle("slow");
+        });
+    });
+
+    $(document).ready(function() {
+        $('#lolgp').click(function() {
+                $('#gplus-button').fadeToggle("fast");
+        });
+    });
+
+    $(document).ready(function() {
+        $('#lol').click(function() {
+                $('#twitter-button').fadeToggle("fast");
+        });
+    });
+
+    $(document).ready(function() {
+        $('#lolfb').click(function() {
+                $('#facebook-button').fadeToggle("fast");
+        });
+    });
+
